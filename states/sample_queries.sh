d4b8f109b85c3ce44c66587b9ba703cea17bdd1f
Igor Motov <igor@motovs.org>
2013-07-24 20:46:26 -0400

    Add sample queries

--- states/sample_queries.sh
+++ states/sample_queries.sh
@@ -1,11 +1,80 @@
-curl -s 'localhost:9200/test-idx/cities/_search?pretty&fields=city,state' -d '{
+curl -s 'localhost:9200/test-data/cities/_search?pretty&fields=city,state' -d '{
     "sort": [
         {"state": {"order": "asc"}},
         {"population2010": {"order": "desc"}}
         
     ]
 }'
-
-# add state region for faceting
-# average population by region
-# index moto using snowball (facet for the most frequently used words in motto)
+echo
+echo "----------------------------------------------------------------------"
+curl 'localhost:9200/test-data/states/_search?pretty' -d '{
+    "explain": false,
+    "query": {
+        "query_string": {
+            "query": "equal",
+            "fields": ["motto", "nicknames^7"],
+            "use_dis_max": true
+        }
+    }
+}'
+echo
+echo "----------------------------------------------------------------------"
+echo "Return all cities that have states that start with A"
+curl 'localhost:9200/test-data/states/_search?pretty' -d '{
+    "query": {
+        "match": {
+            "tree": "pine"
+        }
+    }
+}'
+echo
+echo "Return all cities that start with C in states that have pine as a state tree"
+curl 'localhost:9200/test-data/cities/_search?pretty' -d '{
+    "query": {
+        "bool": {
+            "must": [{
+                "wildcard": {
+                    "city": "c*"
+                }
+            }, {
+                "has_parent": {
+                    "type": "states",
+                    "query":{
+                        "match": {
+                            "tree": "pine"
+                        }
+                    }
+                }
+            }]
+        }
+    }
+}'
+echo
+echo "Return states with pine as a state tree that have cities that start with C"
+curl 'localhost:9200/test-data/states/_search?pretty' -d '{
+    "query": {
+        "bool": {
+            "must": [{
+                "match": {
+                    "tree": "pine"
+                }
+            }, {
+                "has_child": {
+                    "type": "cities",
+                    "query":{
+                        "wildcard": {
+                            "city": "c*"
+                        }
+                    }
+                }
+            }]
+        }
+    }
+}'
+echo
+echo "And now combine these two together"
+curl 'localhost:9200/test-data/_msearch?pretty' -d '{"type" : "states"}
+{"query": {"bool": {"must": [{"match": {"tree": "pine"}}, {"has_child": {"type": "cities", "query":{"wildcard": {"city": "c*"}}}}]}}}
+{"type" : "cities"}
+{"query": {"bool": {"must": [{"wildcard": {"city": "c*"}}, {"has_parent": {"type": "states", "query":{"match": {"tree": "pine"}}}}]}}}
+'
