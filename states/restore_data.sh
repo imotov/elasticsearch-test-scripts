2f4aa8aa364454c7f51df1f1bb96014b06dfa044
Igor Motov <igor@motovs.org>
2013-12-11 22:25:08 -0500

    Add wait for yellow status to make sure index fully started

--- states/restore_data.sh
+++ states/restore_data.sh
@@ -1,15 +1,17 @@
-# Register repository
+echo "Registering repository"
 curl -XPUT 'http://localhost:9200/_snapshot/es_demo_data?pretty' -d '{
     "type": "url",
     "settings": {
         "url": "http://imotov.s3-website-us-west-2.amazonaws.com/demo/"
     }
 }'
-# Restore demo index
+echo "Restoring demo index"
 curl -XPOST "http://localhost:9200/_snapshot/es_demo_data/test/_restore?wait_for_completion=true&pretty" -d '{
     "indices": "large_us_cities"
 }'
-# Run test search
+echo "Waiting for demo index to start"
+curl -XGET "http://localhost:9200/_cluster/health/large_us_cities?wait_for_status=yellow&pretty"
+echo "Test search"
 curl -XGET "http://localhost:9200/large_us_cities/cities/_search?pretty" -d '{
     "query": {
         "range": {
@@ -18,5 +20,6 @@ curl -XGET "http://localhost:9200/large_us_cities/cities/_search?pretty" -d '{
                 "to": 1000000
             } 
         }
-    }
+    },
+    "fields": [ "state", "city", "population2012" ]
 }'
