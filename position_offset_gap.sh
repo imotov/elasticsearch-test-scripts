7878cab58b0096cfe5ce5d4ead375e784b87d2f8
Igor Motov <igor@motovs.org>
2014-02-24 11:17:23 -0500

    Add example of copy_to field

--- position_offset_gap.sh
+++ position_offset_gap.sh
@@ -15,7 +15,12 @@ curl -XPUT localhost:9200/test-idx -d '{
                 "non_zero_gap" : {
                     "type": "string",
                     "position_offset_gap": 256
+                },
+                "copy_field": {
+                    "type": "string",
+                    "copy_to": ["zero_gap", "non_zero_gap"]
                 }
+                
             }
         }
     }
@@ -23,11 +28,13 @@ curl -XPUT localhost:9200/test-idx -d '{
 echo
 curl -XPUT localhost:9200/test-idx/doc/1 -d '{
     "zero_gap": ["foo", "bar"],
-    "non_zero_gap": ["foo", "bar"]
+    "non_zero_gap": ["foo", "bar"],
+    "copy_field": ["bar", "baz"]
 }'
 echo
 curl -XPOST localhost:9200/test-idx/_refresh
 echo
+echo "Without gap"
 curl "localhost:9200/test-idx/doc/_search?pretty=true" -d '{
     "query": {
         "match_phrase" : {
@@ -36,10 +43,29 @@ curl "localhost:9200/test-idx/doc/_search?pretty=true" -d '{
     }
 }'
 echo
+echo "With gap"
 curl "localhost:9200/test-idx/doc/_search?pretty=true" -d '{
     "query": {
         "match_phrase" : {
             "non_zero_gap" : "foo bar"
+        }
+    }
+}'
+echo
+echo "Without gap - copy field"
+curl "localhost:9200/test-idx/doc/_search?pretty=true" -d '{
+    "query": {
+        "match_phrase" : {
+            "zero_gap" : "bar baz"
+        }
+    }
+}'
+echo
+echo "With gap - copy field"
+curl "localhost:9200/test-idx/doc/_search?pretty=true" -d '{
+    "query": {
+        "match_phrase" : {
+            "non_zero_gap" : "bar baz"
         }
     }
 }'
